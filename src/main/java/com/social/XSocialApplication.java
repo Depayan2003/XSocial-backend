84ec36f1db601d5d7b56fff42fb75f26ce3d1386
Depayan2003 <debnathdepayan0@gmail.com>
2026-02-11 13:55:15 +0530

    Converted WAR to JAR and cleaned config

--- src/main/java/com/social/XSocialApplication.java
+++ src/main/java/com/social/XSocialApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class XSocialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XSocialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(XSocialApplication.class, args);
+    }
 
 }
